a94379e3fd4ab7a02e4b271a000e82eeba3526d5
Matross-20 <matrossmthawelanga@gmail.com>
2025-04-05 12:46:25 +0200

    MongoDB utils

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,78 +1,32 @@
-import pkg from 'mongodb';
-const { MongoClient } = pkg;
+const { MongoClient } = require('mongodb');
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
 
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    const uri = `mongodb://${this.host}:${this.port}`;
-    this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    this.db = null;
-  }
-
-  async connect() {
-    try {
-      await this.client.connect();
-      this.db = this.client.db(this.database);
-      console.log('Connected to MongoDB');
-      return true;
-    } catch (err) {
-      console.error('Failed to connect to MongoDB:', err);
-      return false;
-    }
-  }
-
-
-  async isAlive() {
-    if (this.db === null) {
-      const connectionSuccess = await this.connect();
-      if (!connectionSuccess) {
-        return false;
+    MongoClient.connect(url, (err, client) => {
+      if (!err) {
+        this.db = client.db(database);
+      } else {
+        this.db = false;
       }
-    }
-
-    try {
-      await this.db.command({ ping: 1 });
-      return true;
-    } catch (error) {
-
-      console.error('Error pinging MongoDB:', error);
-      return false;
-    }
+    });
   }
 
+  isAlive() {
+    if (this.db) return true;
+    return false;
+  }
 
   async nbUsers() {
-    if (this.db === null) {
-      const connectionSuccess = await this.connect();
-      if (!connectionSuccess) return 0; // Return 0 if connection fails
-    }
-
-    try {
-      const usersCollection = this.db.collection('users');
-      const count = await usersCollection.countDocuments();
-      return count;
-    } catch (err) {
-      console.error('Failed to get user count:', err);
-      return 0;
-    }
+    return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    if (this.db === null) {
-      const connectionSuccess = await this.connect();
-      if (!connectionSuccess) return 0; // Return 0 if connection fails
-    }
-
-    try {
-      const filesCollection = this.db.collection('files');
-      const count = await filesCollection.countDocuments();
-      return count;
-    } catch (err) {
-      console.error('Failed to get file count:', err);
-      return 0;
-    }
+    return this.db.collection('files').countDocuments();
   }
 }
 
